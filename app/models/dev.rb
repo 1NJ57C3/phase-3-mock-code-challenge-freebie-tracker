daef814663cc11064c2e0ea5adaed2f38dd4fb9e
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2022-01-20 02:45:04 -0800

    commit tree ee452efaf6af6814e6e3185948f775c3d7534385

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,13 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        freebies.map{|f|f.item_name.downcase}.include?(item_name.downcase)
+    end
+
+    def give_away(dev, freebie)
+        # freebie.dev == self && freebie.update(dev: dev)
+        freebie.dev == self ? freebie.update(dev: dev) : "#{freebie.dev.name}: \"Hey! That isn't yours!!"
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+c1 = Company.create(name: "Google", founding_year: 1998)
+c2 = Company.create(name: "Facebook", founding_year: 2004)
+c3 = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+c4 = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+d1 = Dev.create(name: "Rick")
+d2 = Dev.create(name: "Morty")
+d3 = Dev.create(name: "Mr. Meseeks")
+d4 = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +17,25 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+
+Freebie.create(item_name: "Baseball Cap", value: 10, company: c1, dev: Dev.all.sample)
+Freebie.create(item_name: "Baseball Cap", value: 10, company: c1, dev: Dev.all.sample)
+Freebie.create(item_name: "Baseball Cap", value: 10, company: c1, dev: Dev.all.sample)
+Freebie.create(item_name: "Baseball Cap", value: 10, company: c1, dev: Dev.all.sample)
+Freebie.create(item_name: "HydroFlask", value: 40, company: c1, dev: Dev.all.sample)
+Freebie.create(item_name: "HydroFlask", value: 40, company: c1, dev: Dev.all.sample)
+Freebie.create(item_name: "T-shirt", value: 25, company: c2, dev: Dev.all.sample)
+Freebie.create(item_name: "T-shirt", value: 25, company: c2, dev: Dev.all.sample)
+Freebie.create(item_name: "T-shirt", value: 25, company: c2, dev: Dev.all.sample)
+Freebie.create(item_name: "Post-its", value: 5, company: c3, dev: Dev.all.sample)
+Freebie.create(item_name: "Post-its", value: 5, company: c3, dev: Dev.all.sample)
+Freebie.create(item_name: "Post-its", value: 5, company: c3, dev: Dev.all.sample)
+Freebie.create(item_name: "Post-its", value: 5, company: c3, dev: Dev.all.sample)
+Freebie.create(item_name: "Hoodie", value: 65, company: c1, dev: Dev.all.sample)
+Freebie.create(item_name: "Hoodie", value: 65, company: c1, dev: Dev.all.sample)
+Freebie.create(item_name: "Hoodie", value: 65, company: c1, dev: Dev.all.sample)
+Freebie.create(item_name: "Hoodie", value: 65, company: c2, dev: Dev.all.sample)
+Freebie.create(item_name: "Hoodie", value: 65, company: c2, dev: Dev.all.sample)
+Freebie.create(item_name: "Bluetooth Speaker", value: 60, company: c4, dev: Dev.all.sample)
 
 puts "Seeding done!"
